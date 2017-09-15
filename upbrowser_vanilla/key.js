1cfd21b9a505efa03e8d1754cc14ca30d9635f09
Dondeo <robin.chatain@epitech.eu>
2017-09-15 17:11:37 +0200

    Update fix Upbrowser

--- upbrowser_vanilla/key.js
+++ upbrowser_vanilla/key.js
@@ -1,5 +1,5 @@
 document.onkeydown = applyKey;
-
+ 
 KEY_DOWN	= 40;
 KEY_UP		= 38;
 KEY_LEFT	= 37;
@@ -56,16 +56,74 @@ REMAP_KEY_T	= 5019;
 
 var size = 0;
 var glob = 0;
-var glob_phrase = 1;
+var glob_phrase = 0;
 var g_pos = 1;
+var p = 0;
+var test = 0;
+var memory = 0;
+var memory_node = "";
 
-function checkEventObj ( _event_ ){
-	// verifions si le navigateur est IE
-	if ( window.event )
-		return window.event;
-	// Si ce n'est pas IE
-	else
-		return _event_;
+function stop_phrase()
+{
+    var childNode = document.body.childNodes;
+    var	i = 0;
+    var txt = childNode[memory].innerHTML;
+    var new_txt = "";
+    while (txt[i] != '<' || txt[i + 1] != 's' || txt[i + 2] != 'p' || txt[i + 3] != 'a' || txt[i + 4] != 'n')
+    {
+	new_txt += txt[i];
+	i = i + 1;
+    }
+    while (txt[i] != '>')
+    {
+	i = i + 1;
+    }
+    i = i + 1;
+    while (txt[i] != '<' || txt[i + 1] != '/' || txt[i + 2] != 's' || txt[i + 3] != 'p' || txt[i + 4] != 'a' || txt[i + 5] != 'n')
+    {
+	new_txt += txt[i];
+	i = i + 1;
+    }
+    while (txt[i] != '>')
+    {
+	i = i + 1;
+    }
+    i = i + 1;
+    while (i < txt.length)
+    {
+	new_txt += txt[i];
+	i = i + 1;
+    }
+    childNode[memory].innerHTML = new_txt;
+    p = 0;
+    
+}
+
+
+function checkEventObj ( _event_ )
+{
+    // verifions si le navigateur est IE
+    if ( window.event )
+	return window.event;
+    // Si ce n'est pas IE
+    else
+	return _event_;
+}
+
+function back()
+{
+    var ii=0;
+    var childNode = document.body.childNodes;
+    while ('phrase' != childNode[ii].className && ii < childNode.length-1)
+    {
+	ii = ii + 1;
+    }
+    while ('phrase' === childNode[ii].className && ii < childNode.length-1)
+    {
+	childNode[ii].className = 'selected';
+	ii = ii + 1;
+
+    }
 }
 
 function applyKey (_event_){
@@ -75,9 +133,13 @@ function applyKey (_event_){
     var intAltKey = winObj.altKey;
     var intCtrlKey = winObj.ctrlKey;
 
+    if (p === 1)
+    {
+	stop_phrase();
+    }
+    back();
     if (intKeyCode == KEY_END)
     {
-	glob_phrase = 1;
 	var x = document.getElementsByClassName("selected");
 	size = size - 1;
 	for (var i = 0; i < x.length; i++)
@@ -104,7 +166,6 @@ function applyKey (_event_){
     }
         if (intKeyCode == KEY_BEGIN)
     {
-	glob_phrase = 1;
 	var x = document.getElementsByClassName("selected");
 	size = size + 1;
 	for (var i = 0; i < x.length; i++)
@@ -156,8 +217,8 @@ function applyKey (_event_){
 	
 	if ( intKeyCode == KEY_RIGHT)
 	{
-	    glob_phrase = 1;
-	var childNode = document.body.childNodes;
+	    	glob_phrase = 0;
+	    var childNode = document.body.childNodes;
 	for (var i = 0; 'selected' != childNode[i].className; i++)
 	{
 	}
@@ -196,7 +257,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("fin de l'element");
+		speakPhrase("fin de l'élément");
 	    }
 	    else
 	    {
@@ -211,8 +272,8 @@ function applyKey (_event_){
 	
     if (intKeyCode == KEY_LEFT )
 	{
-	    glob_phrase = 1;
-	var childNode = document.body.childNodes;
+	    	glob_phrase = 0;
+	    var childNode = document.body.childNodes;
         for (var i = 0; 'selected' != childNode[i].className; i++)
         {
         }
@@ -250,7 +311,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("debut de l'element");
+		speakPhrase("debut de l'élément");
 	    }
 	    else
 	    {
@@ -264,10 +325,10 @@ function applyKey (_event_){
 	
     if ( intKeyCode == KEY_UP)
 	{
-	    glob_phrase = 1;
+	    	glob_phrase = 0;
 	var temp = 1;
-	var poulet = 0;
-	var childNode = document.body.childNodes;
+	    var poulet = 0;
+	    var childNode = document.body.childNodes;
 	for (var i = 1; 'selected' != childNode[i].className; i++)
 	{
         }
@@ -283,19 +344,19 @@ function applyKey (_event_){
 	for(let tmp2=1; tmp2<childNode.length; tmp2++)
 	{
 	    if (childNode[i].className === 'selected')
-		childNode[i].className = 'nop';
+		childNode[i].className = '';
 	}
         for (;i < childNode.length; i--)
         {
             if (childNode[i].nodeName[0] === 'H')
             {
 		document.getElementById(childNode[i].id).scrollIntoView();
-		childNode[temp].className = 'nop';
+		childNode[temp].className = '';
 		temp = temp + 1;
 		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
 		{
 		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
-		    childNode[temp].className = 'nop';
+		    childNode[temp].className = '';
 		    temp = temp + 1;
 		}
                 childNode[i].className = 'selected';
@@ -318,7 +379,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("debut de l'element");
+		speakPhrase("debut de l'élément");
 	    }
 	    else
 	    {
@@ -332,12 +393,12 @@ function applyKey (_event_){
 	
     if ( intKeyCode == KEY_DOWN)
 	{
-	    glob_phrase = 1;
+	    	glob_phrase = 0;
 	var found = false
         var temp = 0;
 	var poulet;
-        var childNode = document.body.childNodes;
-        for (var i = 0; 'selected' != childNode[i].className; i++)
+            var childNode = document.body.childNodes;
+	    for (var i = 0; 'selected' != childNode[i].className; i++)
         {
         }
 	temp = i;
@@ -351,12 +412,12 @@ function applyKey (_event_){
             if (childNode[i].nodeName[0] === 'H')
             {
 		document.getElementById(childNode[i].id).scrollIntoView();
-		childNode[temp].className = 'nop';
+		childNode[temp].className = '';
 		temp = temp + 1;
 		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
 		{
 		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
-		    childNode[temp].className = 'nop';
+		    childNode[temp].className = '';
 		    temp = temp + 1;
 		}
                 childNode[i].className = 'selected';
@@ -379,7 +440,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("fin de l'element");
+		speakPhrase("fin de l'élément");
 	    }
 	    else
 	    {
@@ -393,7 +454,7 @@ function applyKey (_event_){
 	
     if ( intKeyCode == KEY_L)
 	{
-	    glob_phrase = 1;
+	    	glob_phrase = 0;
 	var temp = 1;
 	var childNode = document.body.childNodes;
 	for (var i = 1; 'selected' != childNode[i].className; i++)
@@ -414,18 +475,18 @@ function applyKey (_event_){
 	for(let tmp2=1; tmp2<childNode.length; tmp2++)
 	{
 	    if (childNode[i].className === 'selected')
-		childNode[i].className = 'nop';
+		childNode[i].className = '';
 	}
         for (;i < childNode.length; i--)
             {
             if (childNode[i].nodeName[0] === 'H' && childNode[i].nodeName[1] < current_title)
             {
-		childNode[temp].className = 'nop';
+		childNode[temp].className = '';
 		temp = temp + 1;
 		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
 		{
 		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
-		    childNode[temp].className = 'nop';
+		    childNode[temp].className = '';
 		    temp = temp + 1;
 		}
                 childNode[i].className = 'selected';
@@ -449,7 +510,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("debut de l'element");
+		speakPhrase("debut de l'élément");
 	    }
 	    else
 	    {
@@ -461,7 +522,7 @@ function applyKey (_event_){
 
     if (intKeyCode == KEY_M)
 	{
-	    glob_phrase = 1;
+	    	glob_phrase = 0;
 	var found = false
         var temp = 0;
         var childNode = document.body.childNodes;
@@ -479,12 +540,12 @@ function applyKey (_event_){
         {
             if (childNode[i].nodeName[0] === 'H' && childNode[i].nodeName[1] > current_title)
             {
-		childNode[temp].className = 'nop';
+		childNode[temp].className = '';
 		temp = temp + 1;
 		while (temp < childNode.length && childNode[temp].nodeName[0] !== 'H')
 		{
 		//for (;childNode[temp].nodeName[0] !== 'H'; temp++)
-		    childNode[temp].className = 'nop';
+		    childNode[temp].className = '';
 		    temp = temp + 1;
 		}
                 childNode[i].className = 'selected';
@@ -511,7 +572,7 @@ function applyKey (_event_){
 	    }
 	    if (g_pos === pos)
 	    {
-		speakPhrase("fin de l'element");
+		speakPhrase("fin de l'élément");
 	    }
 	    else
 	    {
@@ -561,33 +622,154 @@ function applyKey (_event_){
 
     if (intKeyCode == KEY_P)
     {
-	var tmp = phrase(document.getElementsByClassName('selected'));
+	p = 1;
+	var childNode = document.body.childNodes;
+	for (var i = 0; 'selected' != childNode[i].className; i++)
+	{
+	}
+	elem = document.getElementsByClassName('selected');
+	memory = i;
+	memory_node = childNode[i].innerHTML;
+	var tmp = childNode[memory].innerHTML;
 	var txt = "";
-	if (glob_phrase >= tmp.length)
+	var cpt = 0;
+	while ('selected' === childNode[i].className)
 	{
-	    glob_phrase = 1;
+	    childNode[i].className = 'phrase';
+	    i = i + 1;
 	}
-	if (glob_phrase === 1 && (tmp[glob_phrase] <= '9' && tmp[glob_phrase] >= '0'))
+	if (glob_phrase+1 >= tmp.length)
 	{
-	    while (((tmp[glob_phrase-1] != '.' && tmp[glob_phrase-1] != '!' && tmp[glob_phrase-1] != '?') || tmp[glob_phrase] != ' ') && glob_phrase < tmp.length)
+	    glob_phrase = 0;
+	}
+	// LECTURE DE LA NUMEROTATION DES TITRES
+	if (glob_phrase === 0 && (tmp[glob_phrase] <= '9' && tmp[glob_phrase] >= '0') && tmp[glob_phrase] === '.')
 	{
-	    console.log(tmp[glob_phrase]);
-	    txt += tmp[glob_phrase];
-	    glob_phrase = glob_phrase + 1;	    
+	    var parse = glob_phrase;
+	    var parse2= 0;
+	    var new_txt = "";
+	    while (tmp[glob_phrase] != ' ' && glob_phrase < tmp.length)
+	    {
+		//txt += tmp[glob_phrase];
+		glob_phrase = glob_phrase + 1;
+	    }
+	    var txt2 = childNode[memory].innerHTML;
+	    txt = "";
+	    while (parse2 < parse)
+	    {
+		txt += txt2[parse2];
+		new_txt += txt2[parse2];
+		parse2 = parse2 + 1;
+	    }
+	    new_txt += "<span id='phrase_selected'>"
+	    while (parse < glob_phrase)
+	    {
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
+	    new_txt += "</span>"
+	    while (parse < txt2.length)
+	    {
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
 	}
+	// SI ON EST DANS UNE LISTE
+	else if (childNode[memory].nodeName === "OL")
+	{
+	    while ((tmp[glob_phrase] === ' ' || tmp[glob_phrase] === '\n') && glob_phrase < tmp.length-1)
+		glob_phrase++;
+	    if (tmp[glob_phrase] === '<' && tmp[glob_phrase+1] === '/' && tmp[glob_phrase+2] === 'l' && tmp[glob_phrase+3] === 'i'  && tmp[glob_phrase+4] === '>')
+	    {
+		glob_phrase = glob_phrase + 5;
+	    }
+	    while ((tmp[glob_phrase] === ' ' || tmp[glob_phrase] === '\n') && glob_phrase < tmp.length-1)
+		glob_phrase++;
+	    if (tmp[glob_phrase] === '<' && tmp[glob_phrase+1] === 'l' && tmp[glob_phrase+2] === 'i' && tmp[glob_phrase+3] === '>')
+	    {
+		glob_phrase = glob_phrase + 4;
+	    }
+	    if (glob_phrase >= tmp.length-1)
+		glob_phrase = 0;
+
+	    while ((tmp[glob_phrase] === ' ' || tmp[glob_phrase] === '\n') && glob_phrase < tmp.length-1)
+		glob_phrase++;
+	    if (tmp[glob_phrase] === '<' && tmp[glob_phrase+1] === '/' && tmp[glob_phrase+2] === 'l' && tmp[glob_phrase+3] === 'i'  && tmp[glob_phrase+4] === '>')
+	    {
+		glob_phrase = glob_phrase + 5;
+	    }
+	    while ((tmp[glob_phrase] === ' ' || tmp[glob_phrase] === '\n') && glob_phrase < tmp.length-1)
+		glob_phrase++;
+	    if (tmp[glob_phrase] === '<' && tmp[glob_phrase+1] === 'l' && tmp[glob_phrase+2] === 'i' && tmp[glob_phrase+3] === '>')
+	    {
+		glob_phrase = glob_phrase + 4;
+	    }
+	    
+	    var parse = glob_phrase;
+	    var parse2= 0;
+	    var new_txt = "";
+	    while (glob_phrase+1 < tmp.length && ((tmp[glob_phrase] != '.' && tmp[glob_phrase] != '!' && tmp[glob_phrase] != '?') || tmp[glob_phrase+1] != ' '))
+	    {
+		txt += tmp[glob_phrase];
+		glob_phrase = glob_phrase + 1;
+	    }
+	    var txt2 = childNode[memory].innerHTML;
+	    while (parse2 < parse)
+	    {
+		new_txt += txt2[parse2];
+		parse2 = parse2 + 1;
+	    }
+	    new_txt += "<span id='phrase_selected'>"
+	    while (parse <= glob_phrase)
+	    {
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
+	    new_txt += "</span>"
+	    while (parse < txt2.length)
+	    {
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
 	}
+	//SELECTION DU RESTE
 	else
 	{
-	while (tmp[glob_phrase-1] != '.' && tmp[glob_phrase-1] != '!' && tmp[glob_phrase-1] != '?' && glob_phrase < tmp.length)
-	{
-	    console.log(tmp[glob_phrase]);
-	    txt += tmp[glob_phrase];
-	    glob_phrase = glob_phrase + 1;	    
+	    var parse = glob_phrase;
+	    var parse2= 0;
+	    var new_txt = "";
+	    while (glob_phrase+1 < tmp.length && ((tmp[glob_phrase] != '.' && tmp[glob_phrase] != '!' && tmp[glob_phrase] != '?') || tmp[glob_phrase+1] != ' '))
+	    {
+		//txt += tmp[glob_phrase];
+		glob_phrase = glob_phrase + 1;
+	    }
+	    var txt2 = childNode[memory].innerHTML;
+	    while (parse2 < parse)
+	    {
+		new_txt += txt2[parse2];
+		parse2 = parse2 + 1;
+	    }
+	    txt = "";
+	    new_txt += "<span id='phrase_selected'>"
+	    while (parse <= glob_phrase)
+	    {
+		txt += txt2[parse];
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
+	    new_txt += "</span>"
+	    while (parse < txt2.length)
+	    {
+		new_txt += txt2[parse];
+		parse = parse + 1;
+	    }
 	}
-	}
+	//LECTURE DE LA PHRASE ET MODIFICATION DU TEXT POUR METTRE LES BALISES SPAN
+	childNode[memory].innerHTML = new_txt;
 	glob_phrase = glob_phrase + 1;
 	winObj.keyCode = intKeyCode = REMAP_KEY_T;
 	winObj.returnValue = false;
+	console.log(txt);
 	speakPhrase(txt);
 	return false;
     }
@@ -604,7 +786,6 @@ function applyKey (_event_){
 	{
 	    while ((tmp[glob_phrase-1] != '.' || tmp[glob_phrase] != ' ') && glob_phrase < tmp.length)
 	{
-	    console.log(tmp[glob_phrase]);
 	    txt += tmp[glob_phrase];
 	    glob_phrase = glob_phrase + 1;	    
 	}
@@ -614,7 +795,6 @@ function applyKey (_event_){
 	{
 	while (tmp[glob_phrase-1] != ','&& tmp[glob_phrase-1] != ';' && tmp[glob_phrase-1] != ' ' && glob_phrase < tmp.length)
 	{
-	    console.log(tmp[glob_phrase]);
 	    txt += tmp[glob_phrase];
 	    glob_phrase = glob_phrase + 1;	    
 	}
@@ -628,10 +808,16 @@ function applyKey (_event_){
 	
 }
 
+function phrase_spe(myText) {
+    var tts = "";
+    tts += ' ' + myText[0].innerText;
+    return (tts);
+}
+
+
 function phrase(myText) {
     var tts = "";
     var textLength = myText.length;
-
     for(var i = 0; i < textLength; i++) {
 	if (i !== 0) {
 	    tts += '.';
